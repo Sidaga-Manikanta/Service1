c39db804e4dccc750e54ab8a27bcd92853f6b541
Manikanta <manikantasidaga@gmail.com>
2024-08-30 13:30:12 +0530

    issue with the session retreving cleared

--- src/main/java/com/capstone/service1/Controllers/UserController.java
+++ src/main/java/com/capstone/service1/Controllers/UserController.java
@@ -27,21 +27,29 @@ public class UserController {
     public boolean checkUser(@RequestParam("mobileNumber")long userNumber, HttpServletRequest request) {
         boolean exists = userService.userExists(userNumber);
         if (exists) {
-            HttpSession session = request.getSession(true);
-            session.setAttribute("userNumber", userNumber);
-            currentUser = userNumber;
+            HttpSession session = request.getSession();
+            session.setAttribute("user", userNumber);
+            System.out.println("session created with id "+ session.getId()+ "current user"+session.getAttribute("user"));
         }
         return exists;
 
     }
     @GetMapping("/currentUser")
-    public long getCurrentUserI() {
-        return currentUser;
+    public long getCurrentUserI(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        session.getAttribute("user");
+        if (session != null) {
+            currentUser = (long) session.getAttribute("user");
+            return currentUser;
+        }
+        return 0;
     }
 
     @PostMapping("/connectWithUs")
     public String sendingConnectionMail(){
         connectWithUsEmail.sendConnectionEmail("s.mani.k1437@gmail.com","about request to our service","the user with details ..want to connect with us");
         return "email sent successfully";
+        
     }
 }
